5a77712996576c7732950d081586328030eee723
Progress Aienobe <paienobe@gmail.com>
2023-11-19 12:53:45 +0100

    initial

---
+++ components/Navbar/Navbar.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import styles from "./Navbar.module.scss";
+import Image from "next/image";
+import logo from "@/assets/shared/desktop/logo.svg";
+import Link from "next/link";
+
+const Navbar = () => {
+  return (
+    <nav className={styles.navbar}>
+      <Image src={logo} alt="logo" />
+
+      <ul className={styles.navbar__link_list}>
+        <li>
+          <Link href="/stories">STORIES</Link>
+        </li>
+        <li>
+          <Link href="/features">FEATURES</Link>
+        </li>
+        <li>
+          <Link href="/pricing">PRICING</Link>
+        </li>
+      </ul>
+
+      <button>GET AN INVITE</button>
+    </nav>
+  );
+};
+
+export default Navbar;
